0413ba80456442aaaaa8665c2a9c0bf3d0c854c1
excelenter <excelenter@yandex.ru>
2012-02-26 04:07:29 +0400

    First commit

---
+++ sshmnt.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+# sshmnt.sh
+#
+# Script that automatically mounts ssh directories into $SSHFSROOT 
+# directory.
+#
+# $SSHFSROOT is environmental variable.
+
+# Text color variables
+txtbld=$(tput bold)					# Bold
+txtemph=$(tput setaf 6)				# Emphasis
+bldred=${txtbld}$(tput setaf 1)		# Red
+bldgreen=${txtbld}$(tput setaf 2)	# Green
+bldyellow=${txtbld}$(tput setaf 3)	# Yellow
+bldwhite=${txtbld}${txtrst}			# White
+txtrst=$(tput sgr0)					# Reset
+
+# Message templates
+info="${bldwhite}[INFO]${txtrst}: "
+warning="${bldyellow}[WARN]${txtrst}: "
+error="${bldred}[FATAL]${txtrst}: "
+success="${bldgreen}[OK]${txtrst}: "
+
+# Check args
+if [ ! $1 ] 
+then
+	echo "${info}Usage: ${txtbld}$(basename $0) ${txtrst}${txtemph}[user@server.com]${txtrst}"
+	exit
+fi
+
+# Check base directory variable
+echo "${info}Checking base directory..."
+if [ ! $SSHFSROOT ]
+then
+	echo "${warning}${txtemph}\$SSHFSROOT${txtrst} variable doesn't exist or empty."
+	echo "${info}Using ${txtemph}$HOME/sshfs/${txtrst} as the base directory."
+	SSHFSROOT="$HOME/sshfs/"
+fi
+# And if neccessary, try to create it
+if [ -d "$SSHFSROOT" ]
+then
+	echo "${success}Base directory ${txtemph}$SSHFSROOT ${txtrst}exists."
+else
+	echo "${warning}Base directory ${txtemph}$SSHFSROOT ${txtrst}doesn't exists."
+	echo "${info}Creating ${txtemph}$SSHFSROOT ${txtrst}directory..."
+	if mkdir "$SSHFSROOT" 2>/dev/null
+	then
+		echo "${success}Base directory ${txtemph}$SSHFSROOT ${txtrst}was succesfully created."
+	else
+		echo "${error}Base directory cannot be created."
+		exit
+	fi
+fi
+# Check server variable
+# And if neccessary, try to create it
+echo "${info}Checking ${txtemph}$1 ${txtrst}directory..."
+if [ -d "$SSHFSROOT$1" ]
+then
+	echo "${success}Directory ${txtemph}$1 ${txtrst}exists." 
+else 
+	echo "${info}Directory ${txtemph}$1 ${txtrst}doesn't exist."
+	echo "${info}Creating ${txtemph}$1 ${txtrst}directory..."
+	if mkdir "$SSHFSROOT$1" 2>/dev/null
+	then
+		echo "${success}Directory ${txtemph}$1 ${txtrst}was successfully created."
+	else
+		echo "${error}Directory creation error (maybe it was mounted before?)"
+		exit
+	fi
+fi
+
+echo "${info}Mounting ${txtemph}$1 ${txtrst}over ssh..."
+if sshfs "$1": "$SSHFSROOT$1" 2> /dev/null
+then
+	echo "${success}Mounted succesfully." 
+else 
+	echo "${error}Mount error." 
+fi
